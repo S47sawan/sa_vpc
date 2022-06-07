a2bf6180c0d83ee1408914e968fe466138862fa4
s47sawan <smihah270@yahoo.com>
2022-06-07 16:14:53 +0100

    commit

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = var.ami[var.region]
+  ami                         = var.bastion_ami[var.region]
   instance_type               = var.bastion_instance_type
   key_name                    = var.key_name
   monitoring                  = true
